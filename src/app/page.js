95efde6e82a7f2a903a47ff21106490af31c9793
Kamrul Hasan <kamrul444216@gmail.com>
2023-07-03 04:42:43 +0600

    banner and about

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import Banner from '@/components/Banner'
+import About from '@/components/About/About'
+import Banner from '@/components/Banner/Banner'
 import Image from 'next/image'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
      <div>Projects</div>
      <Banner/>
+     <About/>
     </main>
   )
 }
