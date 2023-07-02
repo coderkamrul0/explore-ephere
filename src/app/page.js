3dd0b34a23626f6fd8f493e6b4ab069c16adb357
Kamrul Hasan <kamrul444216@gmail.com>
2023-07-02 15:08:31 +0600

    kamrul branch create

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Banner from '@/components/Banner'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
      <div>Projects</div>
+     <Banner/>
     </main>
   )
 }
