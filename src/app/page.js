559605b8747c85c13483254680859d1acfa27358
semon69 <smdemon3@gmail.com>
2023-07-03 16:38:54 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 import Achievements from "@/components/Achivement/page";
 import DestinationList from "@/components/DestinationList/page";
 import RecentNews from "@/components/RecentNews/page";
 
-=======
 
 import Image from 'next/image'
->>>>>>> 6a3134052080ff1731b080c0a28ca8117387bab7
 
 import PopularActivities from '@/components/PopularActivities/PopularActivities'
 import FeaturedTours from '@/components/FeaturedTours/FeaturedTours'
@@ -16,18 +13,15 @@ export default function Home() {
 
   return (
     <main>
-<<<<<<< HEAD
       <DestinationList></DestinationList>
       <RecentNews></RecentNews>
       <Achievements></Achievements>
-=======
 
       {/* popular activities */}
       <PopularActivities></PopularActivities>
       <FeaturedTours></FeaturedTours>
       <Ready></Ready>
 
->>>>>>> 6a3134052080ff1731b080c0a28ca8117387bab7
     </main>
   )
 }
