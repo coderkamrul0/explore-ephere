ec1ceedb6e9704bb71b23f3faec852744bf37d17
semon69 <smdemon3@gmail.com>
2023-07-05 23:12:50 +0600

    add searchpage

---
+++ src/components/SearchPage/ExploreWorld.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const ExploreWorld = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <p>Explore The World</p>
+                    <p>People Don’t Take, Trips Take People</p>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                            <p>++</p>
+                            <div>
+                                <p>Destination</p>
+                                <p>Location</p>
+                            </div>
+                        </div>
+                        <div>
+                            <select className="select select-ghost w-full max-w-xs">
+                                <option disabled selected>Pick the best JS framework</option>
+                                <option>Svelte</option>
+                                <option>Vue</option>
+                                <option>React</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <p>++</p>
+                            <div>
+                                <p>Destination</p>
+                                <p>Location</p>
+                            </div>
+                        </div>
+                        <div>
+                            <select className="select select-ghost w-full max-w-xs">
+                                <option disabled selected>Pick the best JS framework</option>
+                                <option>Svelte</option>
+                                <option>Vue</option>
+                                <option>React</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div>
+                        <p>++</p>
+                        <div>
+                            <p>Destination</p>
+                            <p>Location</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>++</p>
+                        <div>
+                            <p>Destination</p>
+                            <p>Location</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ExploreWorld;
