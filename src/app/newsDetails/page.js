d7d337c435b908630ff22f1d83146de7c7abcc0c
semon69 <smdemon3@gmail.com>
2023-07-18 00:38:05 +0600

    create news details

--- src/app/newsDetails/page.js
+++ src/app/newsDetails/page.js
@@ -3,8 +3,11 @@ import React from 'react';
 const page = () => {
     return (
         <div className='pt-20'>
-            <div>
-                <p className=''>Banner</p>
+            <div className='text-white bg-center bg-no-repeat bg-stone-500 bg-blend-multiply bg-cover h-[450px]' style={{ backgroundImage: "url('https://gaviaspreview.com/wp/gowilds/wp-content/uploads/2023/01/tour-5.jpg')" }}>
+                <div className='text-center p-32'>
+                    <p className='text-3xl lg:text-5xl font-bold pb-4 pt-10'>Things To See And Do When Visiting Japan</p>
+                    <p className=''>Home / Adventure</p>
+                </div>
             </div>
             <div className='flex'>
                 <div className='w-2/3'>
