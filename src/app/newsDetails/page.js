566ec46b0f5ce0d8c558be3d7aa4701c6c1e7b84
semon69 <smdemon3@gmail.com>
2023-07-18 00:04:24 +0600

    add news details page

---
+++ src/app/newsDetails/page.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <div>
+                <p>Banner</p>
+            </div>
+            <div className='flex'>
+                <div className='w-2/3'>
+                    
+                </div>
+                <div className='w-1/3'>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default page;
