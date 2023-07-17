0bbf5ec7501daa41c0a4bbb6ddc7b78850769341
semon69 <smdemon3@gmail.com>
2023-07-17 22:22:37 +0600

    fixed top padding on searchPage route

--- src/app/searchPage/page.js
+++ src/app/searchPage/page.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div>
+        <div className='pt-20'>
             <ExploreWorld></ExploreWorld>
             <FeaturedTourSearch></FeaturedTourSearch>
             <CompanyIntro></CompanyIntro>
