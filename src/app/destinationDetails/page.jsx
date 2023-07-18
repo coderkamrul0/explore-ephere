2b8f32be970e07b3970c4ef27c3a735505f0036e
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-07-18 20:21:20 +0600

    details component added

---
+++ src/app/destinationDetails/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Details from './Details/Details';
+
+const page = () => {
+    return (
+        <div>
+            <Details></Details>
+        </div>
+    );
+};
+
+export default page;
